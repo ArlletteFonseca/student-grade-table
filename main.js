e81c70fd135a28e7795b34bddb6f9f6feca15e40
Arllette Fonseca <arllette.fonseca@gmail.com>
2020-08-22 12:52:36 -0700

    added PageHeader class to components

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var tableElement=document.getElementsByClassName("grading-table")[0];
 var gradeTable=new GradeTable(tableElement);
-var grades=new App(gradeTable);
+var headerElement=document.getElementsByClassName("page-header")[0];
+var pageHeader=new PageHeader(headerElement);
+var grades=new App(gradeTable,pageHeader);
 grades.start();
